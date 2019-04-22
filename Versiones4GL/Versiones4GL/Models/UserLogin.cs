36ad7bedcf573caaeb5347d982ea2dcd4446bae8
diego <diegog252@hotmail.com>
2019-04-22 20:23:59 +0200

    starting apiservice for login

---
+++ Versiones4GL/Versiones4GL/Models/UserLogin.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Versiones4GL.Models
+{
+
+    class UserLogin
+    {
+        [JsonProperty(PropertyName = "nombre")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "nivel")]
+        public string Level { get; set; }
+
+    }
+}
