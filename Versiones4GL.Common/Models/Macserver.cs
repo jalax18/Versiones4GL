918055c283ad608127311b1a69c006d00ab5523b
Jose Antonio lax peñalver <jalax18@gmail.com>
2019-04-24 20:05:03 +0200

    create tables xad, macserver,maccliente,mpecliente.garum

---
+++ Versiones4GL.Common/Models/Macserver.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+
+using System.Collections.Generic;
+
+using System.ComponentModel.DataAnnotations;
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+
+public class Macserver
+
+{
+
+    [Key]
+
+    public int MacserverId { get; set; }
+
+
+
+    [Required(ErrorMessage = "The field {0} is requiered.")]
+
+    [MaxLength(50, ErrorMessage = "The field {0} only can contains a maximum of {1} characters lenght.")]
+
+    [Index("Macserver_Version_Index", IsUnique = true)]
+
+    public string Version { get; set; }
+
+
+
+   // [JsonIgnore]
+
+   // public virtual ICollection<Station> Stations { get; set; }
+
+}
