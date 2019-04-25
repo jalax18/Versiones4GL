1e86bfc584cac7b547d3b1b4474465872158d421
Jose Antonio lax peñalver <jalax18@gmail.com>
2019-04-25 23:40:32 +0200

    activate for changes online in ddbb

---
+++ Versiones4GL.Backend/Migrations/Configuration.cs
@@ -0,0 +1,25 @@
+namespace Versiones4GL.Backend.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Versiones4GL.Backend.Models.LocalDataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationDataLossAllowed = true;
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "Versiones4GL.Backend.Models.LocalDataContext";
+        }
+
+        protected override void Seed(Versiones4GL.Backend.Models.LocalDataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
